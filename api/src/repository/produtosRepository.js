9a52befb9c8419ddb61ff759871faed3d1dd7aba
Alysson Bondade Lopes <bondadelopes2006@gmail.com>
2022-06-15 16:15:34 -0300

    atualizadaaa marcelo

--- api/src/repository/produtosRepository.js
+++ api/src/repository/produtosRepository.js
@@ -5,7 +5,7 @@ export async function cadastrarProduto(produto){
   insert INTO TB_PRODUTO (ID_ADMINISTRADOR, NM_PRODUTO, VL_PRECO, QTD_ESTOQUE, DS_CAPACIDADE, DS_CORES, DS_MEDIDAS)
   values (?, ?, ?, ?, ?, ?, ?)`
 
-  const [resposta] = await con.query(comando, [produto.id, produto.nome, produto.preco, produto.estoque, produto.capacidade, produto.cores, produto.medidas ]) 
+  const [resposta] = await con.query(comando, [produto.idadm, produto.nome, produto.preco, produto.estoque, produto.capacidade, produto.cores, produto.medidas ]) 
   produto.id = resposta.insertId;
   return produto;
 }
