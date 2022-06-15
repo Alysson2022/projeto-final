881a6066eecdbe871807f3501dc0a5c869840354
Alysson Bondade Lopes <bondadelopes2006@gmail.com>
2022-06-15 18:12:51 -0300

    projeto atuaaaalll

--- api/src/controller/alterarController.js
+++ api/src/controller/alterarController.js
@@ -1,4 +1,5 @@
-import { alterarProduto } from "../repository/alterarRepository.js";
+import { alterarProduto, listarProduto, deletarProduto } from "../repository/alterarRepository.js";
+
 
 import { Router } from 'express';
 const server = Router();
@@ -8,14 +9,33 @@ server.put('/produto/:id', async (req, resp) => {
         //const { id,nome, preco, estoque, capacidade, cores, medidas } = req.body;
         const { id } = req.params;
         const produto = req.body;
-        const produtoinserido = await alterarProduto(produto, id);
+        const produtoinserido = await alterarProduto(id, produto);
 
-        resp.send(produtoinserido)
+        resp.status(204).send()
     } catch (err) {
         resp.status(401).send({
             erro: err.message
         })
 
+    }
+})
+
+server.get('/produto', async (req, resp) => {
+    const {nome} = req.query
+    const resposta = await listarProduto(nome);
+    resp.send(resposta);
+})
+
+server.delete('/produto/:id', async (req, resp) => {
+    try{
+        const { id } = req.params;
+        const resposta = await deletarProduto(id);
+
+        resp.send(resposta);
+    } catch(err) {
+        resp.status(401).send({
+            erro: err.message
+        })
     }
 })
 
--- api/src/repository/alterarRepository.js
+++ api/src/repository/alterarRepository.js
@@ -1,6 +1,6 @@
 import { con } from './connection.js'
 
-export async function alterarProduto(produto, id) {
+export async function alterarProduto(id, produto) {
     const comando = `
     update tb_produto 
     set     nm_produto              = ?,
@@ -13,4 +13,29 @@ export async function alterarProduto(produto, id) {
 
     const [resposta] = await con.query(comando, [produto.nome, produto.preco, produto.estoque, produto.capacidade, produto.cores, produto.medidas, id]);
     return resposta.affectedRows;
+}
+
+export async function listarProduto(nome) {
+    const comando = 
+    `select id_produto			id,
+            nm_produto			nome,
+            vl_preco		    valor,
+            qtd_estoque     quantidade,
+            ds_capacidade	  capacidade,
+            ds_cores 			  cores,
+            ds_medidas			medidas
+       from tb_produto
+      where nm_produto			like ?`
+  
+    const [resposta] = await con.query(comando, [`%${nome}%`])
+    return resposta
+  }
+
+  export async function deletarProduto(id) {
+    const comando = 
+    `delete from tb_produto
+           where id_produto = ?`
+
+    const [resposta] = await con.query(comando, [id]);
+    return resposta;
 }
